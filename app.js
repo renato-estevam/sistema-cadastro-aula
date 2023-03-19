ba43a465421cded5b371e20324d2b6c3304b7d7e
renato-estevam <127259956+renato-estevam@users.noreply.github.com>
2023-03-19 12:05:57 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,3 +2,5 @@ console.log("Iniciando a aplicação")
 console.log("Conectando ao banco de dados")
 
 console.log("Enviando para o github")
+console.log("Fazendo algo escondido")
+console.log("Só que vai ficar no histórico")
